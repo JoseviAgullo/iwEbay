b9cec6dce4c4f601334326c2167c6816b96a4795
Pablo Gil <pablo@fraktal-design.net>
2014-01-07 13:24:25 +0100

    Versionando index... se me habia puesto el fichero en ignorados, wtf

---
+++ application/views/admin/index.php
@@ -0,0 +1,12 @@
+<?php $this->load->view("inc/cabecera.inc.php") ?>
+
+
+<?php echo anchor('admin/productos', 'Productos | '); ?>
+    <?php echo anchor('admin/Subastas', 'Subastas | '); ?>
+    <?php echo anchor('admin/Tiendas', 'Tiendas | '); ?>
+    <?php echo anchor('admin/Usuarios', 'Usuarios | '); ?>
+    <?php echo anchor('admin/Votos', 'Votos'); ?>
+
+<?php
+	$this->load->view("inc/pie.inc.php")
+ ?>
