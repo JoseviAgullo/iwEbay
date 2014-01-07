ebcf46d84bcd2c4bba5ae1139f84838e6c8f36cb
mesmerismo <ppa2290@gmail.com>
2014-01-07 19:52:18 +0100

    Adecentado un poco el aspecto de la tienda…

--- application/views/tiendas/tienda.php
+++ application/views/tiendas/tienda.php
@@ -8,42 +8,39 @@ $this->load->view("inc/menuLateralTienda.inc.php");
 ?>
 
 
-<div id="desc_tienda"  style="border-style:solid; border-width:1px; float:left; margin-left:10px; padding:3px;">
-   
-    <?php
-    echo('<p>Bienvenido a la tienda de ' . $usuario->userName . '</p>');
-    echo('<p>'. $tienda->descripcion .'</p>')
-    ?>
-    <div id="imag" style="width:150px; height:150px; margin:3px; padding:3px;"> <?php echo $img_perfil ?></div>
-    <?php 
-    if($propietario != ''){
-        echo anchor('tiendas/modificar/'.$tienda->id, 'Modificar tienda');
-    }
 
- ?>
-
-
-</div>
-    <div id="limpieza" style="clear:both;"></div>
-<div id="ultimas_subastas"  style="border-style:solid; border-width:1px; float:left; margin-left:14.85%; padding:3px;">
-	<h2 style="float: center;">Últimas Subastas </h2>
-    <table>
-        <thead>
+<div id="contenido" style="border-style:solid; border-width:1px; float:left; margin-left:10px; padding:3px;">
+    <fieldset>
+        <div id="imag" style="width:150px; height:150px; margin:3px; padding:3px;"> <?php echo $img_perfil ?></div>
+        <?php
+            echo('<p>Bienvenido a la tienda de ' . $usuario->userName . '</p>');
+            echo('<p>'. $tienda->descripcion .'</p>');
+            if($propietario != ''){
+                echo anchor('tiendas/modificar/'.$tienda->id, 'Modificar tienda');
+            }
+        ?>
+    </fieldset>
+    <fieldset>
+        <h2>Últimas Subastas </h2>
+        <table>
+            <thead>
             <tr>
                 <th>Imagen</th><th>Nombre</th><th>Precio</th><th>Detalles</th>
             </tr>
-        </thead>
-        <tbody>
-    <?php foreach($ultimas as $subasta) :?>
-            <tr>
-                <td><img src="imagenes/productos/default.jpg"></td>
-                <td><?php echo $subasta->nombre; ?></td>
-                <td><?php echo $subasta->precio_compra_ya . '€' ?></td>
-                <td><?php echo anchor('productos/detalle/'.$subasta->producto_id, 'Detalles') ?></td>
-            </tr>
-    <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            <?php foreach($ultimas as $subasta) :?>
+                <tr>
+                    <td><img src="imagenes/productos/default.jpg"></td>
+                    <td><?php echo $subasta->nombre; ?></td>
+                    <td><?php echo $subasta->precio_compra_ya . '€' ?></td>
+                    <td><?php echo anchor('productos/detalle/'.$subasta->producto_id, 'Detalles') ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </fieldset>
+
 </div>
 
 	<div id="limpieza" style="clear:both;"></div>
