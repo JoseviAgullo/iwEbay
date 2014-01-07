ce82a6bfa4466eb5bbec74903c164e0ec34998b5
mapaux <josevi.agullo@gmail.com>
2014-01-07 20:05:32 +0100

    Supuestamente al crear producto deja elegir bien la fecha

--- application/views/productos/nuevo.php
+++ application/views/productos/nuevo.php
@@ -1,6 +1,38 @@
 <?php $this->load->view("inc/cabecera.inc.php") ?>
 <?php $this->load->view("inc/menuLateral.inc.php") ?>
 
+<script>
+    jQuery(function ($) {
+        $.datepicker.regional['es'] = {
+            yearRange: '-110:-18',
+            defaultDate: '-18y',
+            closeText: 'Cerrar',
+            prevText: '<Ant',
+            nextText: 'Sig>',
+            currentText: 'Hoy',
+            monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+                'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié;', 'Juv', 'Vie', 'Sáb'],
+            dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+            weekHeader: 'Sm',
+            dateFormat: 'dd-mm-yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['es']);
+    });
+    $(function () {
+        $("#datepicker").datepicker({
+            firstDay: 1
+        });
+    });
+</script>
+
 <?php
 $error = $this->session->flashdata('error_subasta');
 if($error != '') {
